f692e29adc50437cf15c36c9d98979bf54165f19
Walid Ayadi <walid@tunpixel.com>
2017-01-01 01:30:35 +0100

    patching the index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 let  path = require('path');
-let Matrix = require(path.join(__dirname, '.', './src/core/matrix'));
 let Poly = require(path.join(__dirname, '.', './src/ml/linearRegression/gradientDescent/poly'));
  
-exports.Matrix = Matrix;
 exports.GradDescent = Poly;
