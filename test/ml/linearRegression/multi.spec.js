0100b310bbeaa52c13d6df64ea0be5fce02a3869
Walid Ayadi <walid@tunpixel.com>
2017-01-03 23:19:28 +0100

    added the multivariate example

--- test/ml/linearRegression/multi.spec.js
+++ test/ml/linearRegression/multi.spec.js
@@ -28,41 +28,41 @@ var assert = require('assert');
 // 		}); 
 //   }); 
  
-  describe('converge Multivariate', ()  =>{
-	  
-		let a = 2.289; let b = 2.788; let c = 1.723; let d = 3.778; let e = 2.180;
-		let x = Matrix.random(4, 20);
-		let xx = [];
-		// console.log(x);
-		var y = (xi)=>{
-			return e + d*xi[3] + c*xi[2] + b*xi[1] + a*xi[0]  ;
-		}
-
-		for(let i =0; i < x.rows; i++){
-			let row = x.data[i];
-			xx.push({x:row, y:y(row) })
-		} 
-
-		//console.log(xx)
-		it( 'should return [' + a + ',' + b + '] when calling converge(xx  )'  ,  () => {
-			assert.deepEqual([e,a,b,c,d],Multivariate.converge(xx));
-		}); 
-  }); 
 //   describe('converge Multivariate', ()  =>{
 	  
-// 		let _a = 2.289; 
-// 		let _b = 11.788; 
+// 		let a = 2.289; let b = 2.788; let c = 1.723; let d = 3.778; let e = 2.180;
+// 		let x = Matrix.random(4, 20);
+// 		let xx = [];
+// 		// console.log(x);
+// 		var y = (xi)=>{
+// 			return e + d*xi[3] + c*xi[2] + b*xi[1] + a*xi[0]  ;
+// 		}
 
-// 		let x = [];
-// 		for(let i =1; i < 20; i+=1){
-// 			let k =i;
-// 			let y = _a*k+_b;
-// 			x.push({x: [k], y:y })
+// 		for(let i =0; i < x.rows; i++){
+// 			let row = x.data[i];
+// 			xx.push({x:row, y:y(row) })
 // 		} 
-// 		it( 'should return [' + _b + ',' + _a + '] when calling converge(xx  )'  ,  () => {
-// 			assert.deepEqual([_b,_a],Multivariate.converge(x));
+
+// 		//console.log(xx)
+// 		it( 'should return [' + a + ',' + b + '] when calling converge(xx  )'  ,  () => {
+// 			assert.deepEqual([e,a,b,c,d],Multivariate.converge(xx));
 // 		}); 
 //   }); 
+  describe('converge Multivariate', ()  =>{
+	  
+		let _a = 2.289; 
+		let _b = 11.788; 
+
+		let x = [];
+		for(let i =1; i < 20; i+=1){
+			let k =i;
+			let y = _a*k+_b;
+			x.push({x: [k], y:y })
+		} 
+		it( 'should return [' + _b + ',' + _a + '] when calling converge(xx  )'  ,  () => {
+			assert.deepEqual([_b,_a],Multivariate.converge(x));
+		}); 
+  }); 
 
 
 
